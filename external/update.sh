f16b25ff521297388bae5e32ca6de62221e9693b
Max <max.scheurer@me.com>
2018-01-16 16:47:54 +0100

    update script

--- external/update.sh
+++ external/update.sh
@@ -1,2 +1,12 @@
+CURRENT_DIR=$(PWD)
+DALTON_FULL="dalton_full"
 # Checkout or update dalton_full folder
+if [ -d "$DALTON_FULL" ]; then
+	cd $DALTON_FULL
+	git fetch
+	git pull
+else
+	git clone https://gitlab.com/dalton/dalton.git dalton_full
+fi
+
 # Copy needed files from dalton_full to dalton_gen1int_interface
