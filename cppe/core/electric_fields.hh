c9d4f2599ad6da0a667b2eba86be71ab5efc28f3
Max <max.scheurer@me.com>
2018-03-17 14:58:43 +0100

    added classes for electric fields

---
+++ cppe/core/electric_fields.hh
@@ -0,0 +1,35 @@
+#ifndef LIBCPPE_CPPE_CORE_ELECTRIC_FIELDS_H
+#define LIBCPPE_CPPE_CORE_ELECTRIC_FIELDS_H
+
+class ElectronFiels {
+private:
+  /* data */
+
+public:
+  ElectronFiels ();
+  ~ElectronFiels ();
+};
+
+
+class NuclearFields {
+private:
+  /* data */
+
+public:
+  NuclearFields ();
+  ~NuclearFields ();
+};
+
+
+class MultipoleFields {
+private:
+  /* data */
+
+public:
+  MultipoleFields ();
+  ~MultipoleFields ();
+};
+
+
+
+#endif // LIBCPPE_CPPE_CORE_ELECTRIC_FIELDS_H
