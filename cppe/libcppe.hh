00846e3dcbcee8754770afb3fedbd9c356f06b75
Max Scheurer <max.scheurer@me.com>
2018-12-04 13:40:47 +0100

    optional build of pelib packages, build system cleanup

--- cppe/libcppe.hh
+++ cppe/libcppe.hh
@@ -17,17 +17,18 @@ public:
   CPPE();
   ~CPPE();
   // void call_pe_energy(const double *densmat);
-  // void call_pe_pol_energy(const double *densmat, double* energy);
-  // void call_full_fock(const double* densmat, double* fockmat, double*
-  // energy); bool gen1int_initialized() { return m_gen1int_initialized; } void
-  // initialize_gen1int(int natoms, int nshells, const double* coords, const
-  // double *charges); void gen1int_add_shell(int spher_gto, int idx_center,
-  // const double* coord_center,
-  //   int ang_num, int num_prim, const double* exponents, int num_contr, const
-  //   double* contr_coef);
+  // void call_pe_pol_energy(const double *densmat, double *energy);
+  // void call_full_fock(const double *densmat, double *fockmat, double
+  // *energy); bool gen1int_initialized() { return m_gen1int_initialized; } void
+  // initialize_gen1int(int natoms, int nshells, const double *coords,
+  //                         const double *charges);
+  // void gen1int_add_shell(int spher_gto, int idx_center,
+  //                        const double *coord_center, int ang_num, int
+  //                        num_prim, const double *exponents, int num_contr,
+  //                        const double *contr_coef);
   // void gen1int_print_shells();
-  // void initialize_pelib(PeOptions& options, int natoms, int nbas,
-  //                       const double* coords, const double* charges);
+  // void initialize_pelib(PeOptions &options, int natoms, int nbas,
+  //                       const double *coords, const double *charges);
 
   std::vector<Potential> read_potfile(std::string potfile_name);
 
