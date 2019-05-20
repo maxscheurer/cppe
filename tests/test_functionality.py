89c2930117f739c20eab67f010756c21ebfb1bef
Maximilian Scheurer <max.scheurer@me.com>
2019-05-20 16:45:36 +0200

    added functionality test for isotropic polarizabilities

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -9,9 +9,20 @@ from cppe import Atom, Molecule
 from cppe import PeOptions, CppeState
 
 
+def solve_induced_moments(options, mol):
+    cppe_state = CppeState(options, mol)
+    cppe_state.calculate_static_energies_and_fields()
+
+    static_fields = np.array(cppe_state.get_static_fields())
+    zeros = np.zeros_like(static_fields)
+    cppe_state.update_induced_moments(zeros, False)
+    return cppe_state
+
+
 class TestFunctionality(unittest.TestCase):
     dirname = os.path.dirname(__file__)
     potfile_path = "{}/potfiles/pna_6w.pot".format(dirname)
+    potfile_iso_path = "{}/potfiles/pna_6w_isopol.pot".format(dirname)
     pna_path = "{}/pna.hdf5".format(dirname)
 
     def test_read_potfile(self):
@@ -32,3 +43,33 @@ class TestFunctionality(unittest.TestCase):
         en_el_nuc = cppe_state.get_energies().get("Electrostatic/Nuclear")
         ref = -0.321349401430  # pelib
         np.testing.assert_almost_equal(en_el_nuc, ref, decimal=9)
+
+    def test_iso_pol(self):
+        # use iso_pol option
+        f = h5py.File(self.pna_path, 'r')
+        mol = Molecule()
+        options = PeOptions()
+        options.potfile = self.potfile_path
+        options.iso_pol = True
+        for z, coord in zip(f['atom_charges'], f['atom_coords']):
+            mol.append(Atom(z, *coord))
+
+        induced_test_state = solve_induced_moments(options, mol)
+        induced_test_moments = induced_test_state.get_induced_moments()
+
+        options = PeOptions()
+        options.potfile = self.potfile_iso_path
+        induced_ref_state = solve_induced_moments(options, mol)
+        induced_ref_moments = induced_ref_state.get_induced_moments()
+
+        test_pots = induced_test_state.get_potentials()
+        ref_pots = induced_ref_state.get_potentials()
+
+        for pt, pr in zip(test_pots, ref_pots):
+            for polt, polr in zip(pt.get_polarizabilities(),
+                                  pr.get_polarizabilities()):
+                np.testing.assert_almost_equal(polt.get_values(),
+                                               polr.get_values())
+
+        np.testing.assert_almost_equal(induced_test_moments,
+                                       induced_ref_moments, decimal=8)
