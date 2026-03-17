5875554a5f6f69f46455e879109682f9c81004e0
Maximilian Scheurer <11406414+maxscheurer@users.noreply.github.com>
2026-03-17 19:58:39 +0100

    export __version__ from package root

--- src/cppe/__init__.py
+++ src/cppe/__init__.py
@@ -2,7 +2,18 @@ from .pycppe import *
 from .pycppe import __build_type__, __parallel__
 from .pycppe.tensors import *
 
+try:
+    from ._version import version as __version__
+except Exception:
+    try:
+        from importlib.metadata import version as _pkg_version
+
+        __version__ = _pkg_version("cppe")
+    except Exception:
+        __version__ = "0+unknown"
+
 all = [
     "__build_type__",
     "__parallel__",
+    "__version__",
 ]
